429e8b755f815ee6a5ab7e549b93ec9f333d49dc
Giovannisb <giovannisb59@gmail.com>
2022-05-05 09:54:31 -0300

    criacao das models e configuracao do admin

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from escola.models import *
 
-# Register your models here.
+class Alunos(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'rg', 'cpf', 'data_nascimento')
+    list_display_link = ('id', 'nome')
+    search_field = ('nome',)
+    list_per_page = 20
+
+admin.site.register(Aluno, Alunos)
+
+class Cursos(admin.ModelAdmin):
+    list_display = ('id', 'codigo_curso', 'descricao')
+    list_display_link = ('id', 'codigo_curso')
+    search_field = ('codigo_curso',)
+    
+admin.site.register(Curso, Cursos)
